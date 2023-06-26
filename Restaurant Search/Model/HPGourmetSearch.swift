39c40588af81092b0f8bb1659d405977da0321c9
Yuto Iwakami <11906960+yidev0@users.noreply.github.com>
2023-06-26 09:52:08 +0900

    検索に座標と範囲を追加

--- Restaurant Search/Model/HPGourmetSearch.swift
+++ Restaurant Search/Model/HPGourmetSearch.swift
@@ -15,8 +15,11 @@ class HPGourmetSearch {
     
     public var urlTask: URLSessionTask?
     
-    func search(coordiante: CGPoint, completion: @escaping ([HPShop], Error?) -> Void) {
-        let urlString = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=\(apiKey)&lat=\(coordiante.x)&lng=\(coordiante.y)&count=5&format=json"
+    func search(at coordiante: CLLocationCoordinate2D, range: Int = 3, completion: @escaping ([HPShop], Error?) -> Void) {
+        var urlString: String {
+            let base = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=\(apiKey)"
+            return base + "&lat=\(coordiante.latitude)&lng=\(coordiante.longitude)" + "&range=\(range)" + "&count=5" + "&format=json"
+        }
         
         guard let url = URL(string: urlString) else {
             completion([], nil)
